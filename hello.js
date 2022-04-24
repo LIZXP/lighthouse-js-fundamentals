9ec8f11bab24a2892e4834fc78d7c653a7e991da
LIZXP <peter.xli@outlook.com>
2022-04-24 08:49:00 -0600

    Declaring and Calling Functions first part

---
+++ hello.js
@@ -0,0 +1,11 @@
+/*const sayHello = () => {
+  console.log('Hello World');
+}
+sayHello();*/
+
+const sayHello  = function (name) {
+    console.log("Hello, " + name);
+  }
+  sayHello("Caliban");
+  sayHello("Miranda");
+  sayHello("Ferdinand");
